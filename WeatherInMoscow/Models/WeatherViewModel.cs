70d68cf40fd146fa669fb1f3079e7cbc307e6b57
Artem Vlasov <baneslayer55@gmail.com>
2020-07-01 20:02:14 +0300

    RePush
    removed jokes
    adding paging
    added gitignore(idk how i miss it first time)

---
+++ WeatherInMoscow/Models/WeatherViewModel.cs
@@ -0,0 +1,18 @@
+﻿using PagedList;
+using System.Web.Mvc;
+
+namespace WeatherInMoscow.Models
+{
+    public class WeatherViewModel
+    {
+        public IPagedList<Weather> Weathers { get; set; }
+
+        public int? YearFilter { get; set; }
+
+        public int? MonthFilter { get; set; }
+
+        public SelectList Year { get; set; }
+
+        public SelectList Month { get; set; }
+    }
+}
