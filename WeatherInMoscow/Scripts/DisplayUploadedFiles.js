75d9baa9cbacba0bbe7e96efebe84048fcccceed
Artem Vlasov <baneslayer55@gmail.com>
2020-07-04 17:08:54 +0300

    Script moved to js file

---
+++ WeatherInMoscow/Scripts/DisplayUploadedFiles.js
@@ -0,0 +1,11 @@
+﻿var control = document.getElementById("file-upload");
+control.addEventListener("change", function (event) {
+    var i = 0,
+        files = control.files,
+        len = files.length;
+    document.getElementById('fileList').innerHTML = "Выбранные файлы:<br/>";
+    for (; i < len; i++) {
+        document.getElementById('fileList').innerHTML += files[i].name + "<br/>";
+    }
+
+}, false);
